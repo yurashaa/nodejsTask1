cbabcccc0533a30b539f762761fa278982af0e7f
yurashulechka <yurchik652@gmail.com>
2020-05-19 13:57:56 +0300

    hometask1 cbs

--- moveStudents.js
+++ moveStudents.js
@@ -10,10 +10,13 @@ const buf_path = join(__dirname, 'buf');
 
 function transferFiles(fromP, toP, cb) {
     readdir(fromP, (err, files) => {
+        if(err) return;
         console.log(files);
         files.forEach(file => {
             console.log(file);
-            rename(join(fromP, file), join(toP, file), err => {});
+            rename(join(fromP, file), join(toP, file), err => {
+                console.log(err);
+            });
             console.log("moved");
         });
         console.log('Transfer ended');
@@ -23,6 +26,10 @@ function transferFiles(fromP, toP, cb) {
 
 function moveStudents(path1, path2) {
     mkdir(buf_path, err => {
+        if(err) {
+            console.log(err);
+            return
+        }
         console.log("Dir created");
         transferFiles(path2, buf_path, () => {
             transferFiles(path1, path2, () => {

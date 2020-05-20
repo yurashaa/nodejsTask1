ed56307c896d11a49ed02ced3ccd1c53e19feede
yurashulechka <yurchik652@gmail.com>
2020-05-21 00:37:13 +0300

    clean branch

--- app.js
+++
@@ -1,8 +0,0 @@
-const { join } = require('path');
-
-const moveStudents = require('./moveStudents');
-
-const students18 = "1800";
-const students20 = "2000";
-
-moveStudents(join(__dirname, students20), join(__dirname, students18));
